d3cfda97428182cc30cac135492f64de9476a18f
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-06-28 23:35:14 -0300

    Olá, mundo externo

---
+++ 2 Olá, mundo externo/main.js
@@ -0,0 +1,8 @@
+// O console.log(); é usado na depuração do código, ele imprime o "Olá, Mundo!" no console.
+console.log("Olá, Mundo");
+
+// O alert(); exibe uma caixa de alerta na janela
+alert("Olá, Mundo");
+
+// O document.write(); é usado quando você deseja imprimir o conteúdo no documento HTML.
+document.write("Olá, mundo")
