d5d980d2d97ea1c84014072fd0237058e41f307f
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-07-03 00:03:35 -0300

    Variáveis

---
+++ 4 Variáveis/main.js
@@ -0,0 +1,15 @@
+// Em JavaScript, há três tipos de variáveis: var, let, const
+
+const nome = "Jonatas";
+let idade = 20;
+var resposta = "Meu nome é ";
+
+
+document.write(resposta + nome + " eu tenho " + idade + " de idade");
+
+// diferença entre var e let
+
+/*
+let permite que você declare variáveis limitando seu escopo no bloco, instrução, ou em uma expressão na qual ela é usada. Isso é inverso da keyword var, que define uma variável globalmente ou no escopo inteiro de uma função, independentemente do escopo de bloco.
+*/
+
