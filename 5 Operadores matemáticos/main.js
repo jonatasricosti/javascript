f2ad7e57ccacd683a59221ce3e3b4971f0a138ef
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-07-04 00:26:07 -0300

    Operadores matemáticos

---
+++ 5 Operadores matemáticos/main.js
@@ -0,0 +1,41 @@
+// Operador de atribuição (=)
+
+
+// Operadores matemáticos
+
+document.write(5+2); // + adição
+document.write("<br>"); // pula linha
+
+document.write(5-2); // - subtração
+document.write("<br>"); // pula linha
+
+document.write(5*2); // * multiplicação
+document.write("<br>"); // pula linha
+
+document.write(36/2); // divisão
+document.write("<br>"); // pula linha
+
+document.write(50 % 12); // % módulo ou resto da divisão
+document.write("<br>"); // pula linha
+
+
+/*
+expressão equivalente a ...
+x += y;	x = x + y;
+x -= y;	x = x - y;
+x *= y; x = x * y;
+x /= y;	x = x / y;
+x %= y; x = x % y;
+*/
+
+
+let x = 100;
+let y = 11;
+
+x %= 12
+y *= 3
+
+document.write(x);      // mostra o valor de x
+document.write("<br>"); // pula linha
+document.write(y);      // mostra o valor de y
+document.write("<br>"); // pula linha
