23a41b9b132f959fbe8bcd35414bbbc61becd1be
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-07-04 22:54:07 -0300

    Operadores lógicos

---
+++ 6 Operadores lógicos/main.js
@@ -0,0 +1,95 @@
+/*
+
+Operadores lógicos
+
+> maior que
+< menor que
+>= maior ou igual
+<= menor ou igual
+== igual a
+!= diferente
+
+
+
+
+operadores lógicos de comparação
+&&  e
+||  ou
+!   não
+
+true  = 1 = verdadeiro
+false = 0 = falso
+
+1 = verdadeiro
+0 = falso
+
+
+tabela verdade
+
+// && = e 
+
+true  && true  =  true
+true  && false =  false
+false && true  =  false
+false && false =  false
+
+// || = ou 
+
+true  || true  =  true
+true  || false =  true
+false || true  =  true
+false || false =  false
+
+
+
+// !   não
+
+!false = true
+!true  = false
+
+*/
+
+
+document.write(5 > 2)
+document.write("<br>")
+document.write(2 < 5)
+document.write("<br>")
+document.write(8 >= 7)
+document.write("<br>")
+document.write(7 <= 8)
+document.write("<br>")
+document.write("Jonatas" == "Jonatas")
+document.write("<br>")
+document.write(1 != 11)
+document.write("<br>")
+document.write("<br>")
+
+
+document.write(true  && true)
+document.write("<br>")
+document.write(true  && false)
+document.write("<br>")
+document.write(false && true)
+document.write("<br>")
+document.write(false && false)
+document.write("<br>")
+document.write("<br>")
+
+
+document.write(true  || true)
+document.write("<br>")
+document.write(true  || false)
+document.write("<br>")
+document.write(false || true)
+document.write("<br>")
+document.write(false || false)
+document.write("<br>")
+document.write("<br>")
+
+document.write(!false)
+document.write("<br>")
+document.write(!true)
+
+
+
+
