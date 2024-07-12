5f372339bb0a4db069810c5ee084d67818810db5
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-07-11 22:51:18 -0300

    Operador typeof

---
+++ 9 Operador typeof/main.js
@@ -0,0 +1,27 @@
+// Em JavaScript, o operador typeof retorna o tipo de dado de uma variável.
+
+
+
+
+let a = 10                 // tipo number
+let b = "10"               // tipo string
+let c = a == b             // tipo boolean
+let d = {nome: "Jonatas"}  // tipo object
+
+
+
+document.write(typeof(a))
+document.write("<br>")
+
+document.write(typeof(b))
+document.write("<br>")
+
+document.write(typeof(c))
+document.write("<br>")
+
+document.write(typeof(d))
+document.write("<br>")
+
+
+
+
