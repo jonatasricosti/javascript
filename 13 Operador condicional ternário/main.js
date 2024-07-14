5adf171decc3599933746b4d3fc5041c3cfce156
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-07-14 00:06:27 -0300

    Operador condicional ternário

---
+++ 13 Operador condicional ternário/main.js
@@ -0,0 +1,20 @@
+/*
+Operador condicional ternário
+sintaxe
+expressão ? valor se for verdadeiro : valor se for falso
+1 = true
+0 = false
+*/
+
+5 > 2 ? document.write("Sim") : document.write("Não")
+document.write("<br>")
+1 > 2 ? document.write("Sim") :  document.write("Não")
+document.write("<br>")
+
+
+let valor = "teste"
+5 < 2 ? valor = true: valor = false;
+
+document.write(valor)
+document.write("<br>")
+
