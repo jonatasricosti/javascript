218aa42afc37ab6097ea0a2828c7035d232222f5
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-07-14 22:52:34 -0300

    if

---
+++ 14 if/main.js
@@ -0,0 +1,59 @@
+/*
+if Operador condicional
+
+sintaxe
+if(verdadeiro)
+{
+	verdadeiro
+	executa o código aqui
+}
+
+else
+{
+	falso
+	executa aqui em baixo
+}
+
+*/
+
+
+// exemplos
+if(true)
+{
+	document.write("Eu gosto de javascript")
+	document.write("<br>")
+}
+
+else
+{
+	document.write("Eu não gosto de javascript")
+	document.write("<br>")
+}
+
+
+if(false)
+{
+	document.write("javascript é ruim")
+	document.write("<br>")
+}
+
+else
+{
+	document.write("javascript é bom")
+	document.write("<br>")
+}
+
+
+let a = 5
+let b = 7
+if(a > b)
+{
+	document.write(a + " é maior que " + b)
+	document.write("<br>")
+}
+
+else
+{
+	document.write(a + " é menor que " + b)
+	document.write("<br>")
+}
