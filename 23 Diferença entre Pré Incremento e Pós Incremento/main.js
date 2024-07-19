d8c6220989b73338d4e0c46e37de542d0fb1271b
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-07-19 00:34:29 -0300

    Diferença entre Pré Incremento e Pós Incremento

---
+++ 23 Diferença entre Pré Incremento e Pós Incremento/main.js
@@ -0,0 +1,34 @@
+
+
+// Diferença entre Pré Incremento e Pós Incremento
+
+let a = 10
+document.write(a)
+document.write("<br>")
+
+document.write(a++) // Pós Incremento aumaneta o valor depois
+document.write("<br>")
+
+document.write(a)
+document.write("<br>")
+
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+
+let b = 10
+document.write(b)
+document.write("<br>")
+
+document.write(++b) // Pre Incremento aumenta o valor antes
+document.write("<br>")
+
+document.write(b)
+document.write("<br>")
+
+
+// isso também vale para decremento, ou seja, --valor, valor--
+
+
+
+
