2f1ec60e260bc686ce83e04bc0d75b56cac8d73b
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-07-19 23:15:45 -0300

    alert

---
+++ 24 alert/main.js
@@ -0,0 +1,10 @@
+/*
+O método window.alert() mostra uma caixa de diálogo de aviso
+com o conteúdo opcionalmente especificado e um botão OK.
+*/
+
+
+let texto = "O método alert mostra essa mensagem na janela";
+
+
+window.alert(texto)
