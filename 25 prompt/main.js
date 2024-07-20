fde8c641316596bedfbd215500ff1045a7479cdf
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-07-19 23:29:26 -0300

    prompt

---
+++ 25 prompt/main.js
@@ -0,0 +1,14 @@
+/*
+O método window.prompt() é o comando leia da linguagem javascript
+*/
+
+
+let nome = window.prompt("Qual é o seu nome: ")
+
+let idade = window.prompt("Quantos anos você tem: ")
+
+document.write("Meu nome é: " + nome + "<br>")
+
+document.write("Eu tenho " + idade + " anos de idade" + "<br>")
+
+
