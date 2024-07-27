f5278fd2be5655f64d158155d18da990816b188a
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-07-27 00:23:25 -0300

    código adicionado

--- 27 Matriz/main.js
+++ 27 Matriz/main.js
@@ -2,3 +2,43 @@
 Uma matriz é um array bidimensional, ou seja,
 uma estrutura de dados que contém linhas e colunas
 */
+
+
+let matrix =
+[
+  [10, 20, 30], 
+  [40, 50, 60], 
+  [70, 80, 90] 
+];
+
+
+// exibindo todos os valores da matriz manual
+document.write(matrix[0][0] + " ")    // linha = 0  coluna = 0
+document.write(matrix[0][1] + " ")    // linha = 0  coluna = 1
+document.write(matrix[0][2] + " ")    // linha = 0  coluna = 3
+document.write("<br>")                // pula linha 
+
+document.write(matrix[1][0] + " ")    // linha = 1  coluna = 0
+document.write(matrix[1][1] + " ")    // linha = 1  coluna = 1
+document.write(matrix[1][2] + " ")    // linha = 1  coluna = 3
+
+document.write("<br>")                // pula linha 
+document.write(matrix[2][0] + " ")    // linha = 2  coluna = 0
+document.write(matrix[2][1] + " ")    // linha = 2  coluna = 1
+document.write(matrix[2][2] + " ")    // linha = 2  coluna = 3
+
+
+
+document.write("<br>" + "<br>"+ "<br>")
+
+
+// assim como array matrizes funciona muito bem o loop for
+// exibindo todos os valores da matriz com o loop for
+for(let linha = 0; linha < 3; linha++)
+{
+	document.write("<br>") // pula linha toda vez que muda de linha
+	for(let coluna = 0; coluna < 3; coluna++)
+	{
+		document.write(matrix[linha][coluna] + "  ");
+	}
+}
